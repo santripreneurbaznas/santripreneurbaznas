80323cbc1af0c33fada75a2a45c9394d029a1bb9
rhaihanadzani <mrhaihan17@gmail.com>
2025-07-02 00:08:33 +0700

    build project 1

---
+++ public/build/assets/Index-43c9e170.js
@@ -0,0 +1,1 @@
+import{j as r,F as t}from"./app-56c9d2f2.js";import"./Toater-c262cea3.js";import"./index-825608d7.js";const a=()=>r(t,{children:"Index"});export{a as default};
--- public/build/assets/Index-9c470f07.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as e}from"./app-37a05af3.js";import"./Toater-9ddae612.js";const a=()=>r(e,{children:"Index"});export{a as default};
