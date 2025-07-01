7f4e852ec3c1754352847aea7d665ff9829bff97
rhaihanadzani <mrhaihan17@gmail.com>
2025-07-01 18:29:31 +0700

    build website

--- public/build/assets/Index-2770a56f.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as t}from"./app-cf1c6c17.js";import"./Dropdown-1d1d20e7.js";import"./transition-9d659475.js";const a=()=>r(t,{children:"Index"});export{a as default};
---
+++ public/build/assets/useFlashMessages-a7575ea2.js
@@ -0,0 +1,1 @@
+import{X as e,r as o}from"./app-37a05af3.js";import{t as r}from"./Toater-9ddae612.js";function a(){const{flash:s}=e().props;o.useEffect(()=>{s.error&&r.error(s.error),s.success&&r.success(s.success)},[s])}export{a as u};
