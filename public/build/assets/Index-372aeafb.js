0a25a8d9eadd7d8b174666535b774f660f91eab4
rhaihanadzani <mrhaihan17@gmail.com>
2025-07-02 11:10:33 +0700

    fixed build

---
+++ public/build/assets/Index-372aeafb.js
@@ -0,0 +1,1 @@
+import{j as r,F as t}from"./app-b764cbc1.js";import"./Toater-8444c9b7.js";import"./index-a400f346.js";const a=()=>r(t,{children:"Index"});export{a as default};
--- public/build/assets/useFlashMessages-797ff811.js
+++
@@ -1,1 +0,0 @@
-import{X as e,r as o}from"./app-b9aa26ba.js";import{t as r}from"./index-f40af795.js";function a(){const{flash:s}=e().props;o.useEffect(()=>{s.error&&r.error(s.error),s.success&&r.success(s.success)},[s])}export{a as u};
