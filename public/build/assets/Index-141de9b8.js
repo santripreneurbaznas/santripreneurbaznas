b2f09a295400e6b361940477cdfd521c1a0e32ca
rhaihanadzani <mrhaihan17@gmail.com>
2025-07-02 12:10:31 +0700

    build

--- public/build/assets/Index-141de9b8.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as t}from"./app-f1a8b610.js";import"./Toater-ded24001.js";import"./index-60594ba4.js";const a=()=>r(t,{children:"Index"});export{a as default};
---
+++ public/build/assets/Index-9eef5ac3.js
@@ -0,0 +1,1 @@
+import{j as r,F as t}from"./app-1b229ebe.js";import"./Toater-1202a17a.js";import"./index-b83f3a49.js";const a=()=>r(t,{children:"Index"});export{a as default};
