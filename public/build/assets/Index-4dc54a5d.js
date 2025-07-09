bda832c02738f2c94160b7b18a71819af2e934ac
rhaihanadzani <mrhaihan17@gmail.com>
2025-07-09 18:32:08 +0700

    first commit6

--- public/build/assets/Index-4dc54a5d.js
+++
@@ -1,1 +0,0 @@
-import{j as r,F as t}from"./app-bc0beada.js";import"./Toater-6654b165.js";import"./index-710cf80f.js";const a=()=>r(t,{children:"Index"});export{a as default};
---
+++ public/build/assets/Index-9ca507c2.js
@@ -0,0 +1,1 @@
+import{j as r,F as t}from"./app-15b6aeb1.js";import"./Toater-82e9cfd1.js";import"./index-c69c1dde.js";const a=()=>r(t,{children:"Index"});export{a as default};
